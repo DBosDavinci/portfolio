7f7f0f7e7ac9c8d5597537e737ad58b1299a3f69
DBosDavinci <99069008@mydavinci.nl>
2025-10-02 14:14:53 +0200

    github icon en active item

--- webpage/components/navbar.tsx
+++ webpage/components/navbar.tsx
@@ -1,45 +1,30 @@
+"use client";
+
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
-  NavbarMenu,
-  NavbarBrand,
   NavbarItem,
-  NavbarMenuItem,
 } from "@heroui/navbar";
-import { Kbd } from "@heroui/kbd";
-import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
+import {
+  GithubIcon
+} from "@/components/icons";
+
 import { siteConfig } from "@/config/site";
+import { usePathname } from "next/navigation";
+import { Link } from "@heroui/link";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      type="search"
-    />
-  );
 
   return (
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
+            <NavbarItem key={item.href} isActive={item.href === usePathname()}>
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
@@ -53,6 +38,11 @@ export const Navbar = () => {
             </NavbarItem>
           ))}
         </ul>
+      </NavbarContent>
+      <NavbarContent className="basis-3/5 sm:basis-full" justify="end">
+        <Link isExternal href="https://github.com/DBosDavinci">
+          <GithubIcon className="text-default-500" />
+        </Link>
       </NavbarContent>
     </HeroUINavbar>
   );
