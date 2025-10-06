ee32789fe15a3dc25da06054f74b2cf552eb4927
DBosDavinci <bosdaan15@gmail.com>
2025-10-06 17:27:06 +0200

    translations added

--- webpage/app/[locale]/page.tsx
+++ webpage/app/[locale]/page.tsx
@@ -2,12 +2,15 @@ import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 
 import { title } from "@/components/primitives";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations('Home');
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Daniel Bos Portfolio&nbsp;</span>
+        <span className={title()}>{t('title')}&nbsp;</span>
       </div>
 
       <div className="mt-8">
